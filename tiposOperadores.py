1f0fe6779bf0af03decb843120490f3175ecb273
FabriciaDev <phabymandalas@gmail.com>
2023-04-30 20:22:18 -0300

    mais

--- tiposOperadores.py
+++ tiposOperadores.py
@@ -1,3 +1,26 @@
+# aritméeticods
+
+# adição
+print(1 + 1)
+
+#subtração
+print(10 - 2)
+
+# multiplicação
+print(4 * 3)
+
+# módulo: o resto da divisão
+print(2 ** 3)
+
+# exponenciação
+print(2 ** 3)
+
+# ordem matemática
+# 1.Parêntesis
+# 2.Expoentes
+# 3.Multiplicações e divisões (da esquerda para a direita)
+# 4.Somas e subtração (da esquerda para a direita)
+
 #igualdade
 saldo = 450
 saque = 200
