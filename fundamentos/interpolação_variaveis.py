c2d5ac7b342150231aa3eeed2f54ef5ccd87bad1
FabriciaDev <phabymandalas@gmail.com>
2023-05-13 00:43:58 -0300

    dois minutos dicionario

--- fundamentos/interpolação_variaveis.py
+++ fundamentos/interpolação_variaveis.py
@@ -9,3 +9,5 @@ linguagem = "Python"
 # usa-se s para string, d para inteiro e f para float
 print("Olá, me chamo %s. Eu tenho %d anos de idade, trabalho como %s e estou matriculado no curso de %s." % (nome, idade, profissao, linguagem))
 
+# format coloca {} e, dentro pode estar vasio, seguindo o exemplo de %
+print("Olá, me chamo %s. Eu tenho %d anos de idade, trabalho como %s e estou matriculado no curso de %s." % (nome, idade, profissao, linguagem))
