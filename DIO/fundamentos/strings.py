bdd41fdb839ee7c733b468759f23fc6c43668d1a
FabriciaDev <phabymandalas@gmail.com>
2023-06-04 23:04:56 -0300

    aulastring

--- DIO/fundamentos/strings.py
+++ DIO/fundamentos/strings.py
@@ -10,7 +10,7 @@ print(curso.lower())
 print(curso.title())
 
 # Eiminando espaços em branco
-# Eliminando os espaços em bnrnco dos dois lados
+# Eliminando os espaços em branco dos dois lados
 print(curso.strip())
 # Eliminando os espaços em branco iniciais
 print(curso.lstrip())
@@ -18,8 +18,10 @@ print(curso.lstrip())
 print(curso.rstrip())
 
 # método center: o segundo valor é opicional, ou seja se não colocar ele adiciona um espaço
+# escreva a variável curso centralizada em 10 caracteres e incluído o caractere * onde estiver em branco
 print(curso.center(10, "*"))
 
 # métdo join
+# escreva . para cada item da variável s seguir que é a variável curso
 print(".".join(curso))
 
--- DIO/strings/interpolacao_variaveis.py
+++ DIO/strings/interpolacao_variaveis.py
@@ -1,1 +1,34 @@
-# Aula
+
+nome = "Guilherme"
+idade = 28
+profissao = "Programador"
+linguagem = "Python"
+
+pessoa = {"nome": "Guilherme", "idade": 28, "profissao": "Programador", "linguagem": "Python" }
+
+
+# Old Format : %
+print("Ola, me chamo %s. Tenho %d anos de idade, trabalho como %s, e estou matriculado no curso de %s." % (nome, idade, profissao, linguagem))
+
+# Método Format
+
+# Se usado sem nada dentro ele segue a mesma ordem que são apresentadas as variáveis
+print("Ola, me chamo {}. Tenho {} anos de idade, trabalho como {}, e estou matriculado no curso de {}." .format(nome, idade, profissao, linguagem))
+# Com número dentro , representa a variável na posição correspondente
+print("Ola, me chamo {3}. Tenho {2} anos de idade, trabalho como {1}, e estou matriculado no curso de {0}." .format(profissao, linguagem, idade, nome))
+# Dessa forma é possível determinar "a qual variável" a "variável informada" pertence 
+print("Ola, me chamo {nome}. Tenho {idade} anos de idade, trabalho como {profissao}, e estou matriculado no curso de {linguagem}." .format(nome = nome, idade = idade, profissao = profissao, linguagem = linguagem))
+# Pegando de algum dicionário
+print("Ola, me chamo {nome}. Tenho {idade} anos de idade, trabalho como {profissao}, e estou matriculado no curso de {linguagem}." .format(**pessoa))
+
+# Método f-string
+
+#só coloca o valor dentro das chaves e a letra f antes de tudo
+print(f"Ola, me chamo {nome}. Tenho {idade} anos de idade, trabalho como {profissao}, e estou matriculado no curso de {linguagem}.") 
+# formatando uma string com f-string
+
+PI = 3.14159
+# escreva "valor de PI:" o valor antes do ponto e dois depois
+print(f"Valor de PI: {PI: .2f}")
+# escreva "Valor de PI:" com 10 casas antes do ponto e duas depois
+print(f"Valor de PI: {PI:10.2f}")
